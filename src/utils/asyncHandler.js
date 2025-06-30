9cb591b7a9759fe7444e3aa16b9e2c2edcf3c26b
Akash <akbghsy@gmail.com>
2025-07-01 00:43:29 +0530

    Add register controller

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,28 +1,56 @@
-const asyncHandler = (requestHandler)=>{
-   return (req,res,next)=>{
-   
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-    }
 
+const asyncHandler = function (requestHandler){
+    return async(req,res,next)=>{
+        try {
+            await requestHandler(req,res,next);
+            
+        } catch (error) {
+
+
+            
+        }
+    }
 }
 
 export {asyncHandler}
 
 
+
+
+
+
+
+
+
+
+// const asyncHandler = (requestHandler)=>{
+//    (req,res,next)=>{
+   
+//         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+//     }
+
+// }
+
+
+
 //using async
 
 // export const asyncHandler2 = (func)=>async(req,res,next)=>{
-
-//     try {
-//         await func(req,res,next)
+    
+    //     try {
+        //         await func(req,res,next)
         
-//     } catch (error) {
-//         res.status(err.code || 500).json({
-//             success:false,
-//             message:err.message
-//         })
-        
-//     }
+        //     } catch (error) {
+            //         res.status(err.code || 500).json({
+                //             success:false,
+                //             message:err.message
+                //         })
+                
+                //     }
+                
+                
+                // }
 
 
-// }
+
+
