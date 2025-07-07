009f4887176140f33e814b0c3858b50c52c77cef
Akash <akbghsy@gmail.com>
2025-07-07 10:36:50 +0530

    bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,7 +7,8 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const generateAccessAndRefreshTokens = async (userId)=>{
     try {
-        const user = await User.findById({userId});
+        const user = await User.findById({_id:userId});
+       
        const accessToken = user.generateAccessToken();
        const refreshToken = user.generateRefreshToken();
 
@@ -112,8 +113,9 @@ const loginUser = asyncHandler(async (req,res)=>{
 
 
     const {email,username,password} = req.body
+ 
 
-    if(!username || !email){
+    if(!(username || email)){
         throw new ApiError(400,"username or email is required")
     }
 
