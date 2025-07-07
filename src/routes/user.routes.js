b56f89b7b1af2fa9327a690197c07e2890331873
Akash <akbghsy@gmail.com>
2025-07-07 09:27:45 +0530

    add middleware ,register controller,login controller and logout controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { logoutUser,loginUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post( upload.fields([{name:"avatar",maxCount:1},{name:"coverImage",maxCount:1}]), registerUser)
+router.route("/login").post(loginUser)
+
+
+//  secure routes
+
+router.route("/logout").post(verifyJWT,loginUser)   
 
 
 
